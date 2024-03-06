f1e8bcd7bbc4d10fcd024768addb1c151a241df1
Artem <162103655+artem1dev@users.noreply.github.com>
2024-03-06 15:15:32 +0200

    Finish

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Get, NotFoundException, Param, Post } from "@nestjs/common";
+import { CreateMessageDto } from "./dtos/create-message.dto";
+import { MessagesService } from "./messages.service";
+
+@Controller("messages")
+export class MessagesController {
+    constructor(
+        public messagesService: MessagesService) {}
+
+    @Get()
+    listMessages() {
+        return this.messagesService.findAll();
+    }
+
+    @Get(":id")
+    async getMessage(@Param("id") id: string) {
+        const message = await this.messagesService.findOne(id);
+        if (!message) {
+            throw new NotFoundException("message npt found");
+        }
+        return message;
+    }
+
+    @Post()
+    createMessages(@Body() body: CreateMessageDto) {
+        return this.messagesService.create(body.content);
+    }
+}
